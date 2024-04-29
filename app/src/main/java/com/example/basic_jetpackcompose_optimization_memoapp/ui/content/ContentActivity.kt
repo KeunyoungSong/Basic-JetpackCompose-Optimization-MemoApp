b4d4cc1f50001076774d1a46c6af4f019b28d275
LB-Brandon <rmsdud623@gmail.com>
2024-04-29 13:34:34 +0900

    ContentActivity 를 studio 에서 AppCompatActivity 의 자식으로 생성해줬는데 이를 ComponentActivity 로 변경함
    - 변경하지 않았을 때 에러 생김 (java.lang.IllegalStateException: You need to use a Theme.AppCompat theme (or descendant) with this activity.)

--- app/src/main/java/com/example/basic_jetpackcompose_optimization_memoapp/ui/content/ContentActivity.kt
+++ app/src/main/java/com/example/basic_jetpackcompose_optimization_memoapp/ui/content/ContentActivity.kt
@@ -1,19 +1,25 @@
 package com.example.basic_jetpackcompose_optimization_memoapp.ui.content
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.basic_jetpackcompose_optimization_memoapp.R
+import com.example.basic_jetpackcompose_optimization_memoapp.ui.theme.BasicJetpackComposeOptimizationMemoAppTheme
 
-class ContentActivity : AppCompatActivity() {
+class ContentActivity : ComponentActivity() {
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			ContentScreen(intent.getIntExtra("id",0))
+			ContentScreen(intent.getIntExtra("id", 0))
 		}
 	}
 }
