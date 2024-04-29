82d7a8c1c05ce19d00c55da1d7b825f21d700053
LB-Brandon <rmsdud623@gmail.com>
2024-04-29 13:23:30 +0900

    ContentScreen 추가 및 설정

--- app/src/main/java/com/example/basic_jetpackcompose_optimization_memoapp/ui/content/ContentActivity.kt
+++ app/src/main/java/com/example/basic_jetpackcompose_optimization_memoapp/ui/content/ContentActivity.kt
@@ -1,6 +1,7 @@
 package com.example.basic_jetpackcompose_optimization_memoapp.ui.content
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,12 +12,8 @@ class ContentActivity : AppCompatActivity() {
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		enableEdgeToEdge()
-		setContentView(R.layout.activity_content)
-		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-			insets
+		setContent {
+			ContentScreen(intent.getIntExtra("id",0))
 		}
 	}
 }
