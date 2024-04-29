86a74c91a89f953437f651d50ddb244cb9f0c889
LB-Brandon <rmsdud623@gmail.com>
2024-04-29 13:05:10 +0900

    HomeActivity 추가

--- app/src/main/java/com/example/basic_jetpackcompose_optimization_memoapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/basic_jetpackcompose_optimization_memoapp/ui/home/HomeActivity.kt
@@ -14,10 +14,7 @@ class HomeActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			BasicJetpackComposeOptimizationMemoAppTheme { // A surface container using the 'background' color from the theme
-				Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-				}
-			}
+			HomeScreen(homeState = HomeState(this))
 		}
 	}
 }
