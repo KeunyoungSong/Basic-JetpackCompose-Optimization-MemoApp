04d0c5b69b6d23fac7cc09f6fb40d9ccd6612cea
LB-Brandon <rmsdud623@gmail.com>
2024-04-29 11:42:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/basic_jetpackcompose_optimization_memoapp/ui/home/HomeActivity.kt
@@ -0,0 +1,23 @@
+package com.example.basic_jetpackcompose_optimization_memoapp.ui.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.basic_jetpackcompose_optimization_memoapp.ui.theme.BasicJetpackComposeOptimizationMemoAppTheme
+
+class HomeActivity : ComponentActivity() {
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			BasicJetpackComposeOptimizationMemoAppTheme { // A surface container using the 'background' color from the theme
+				Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+				}
+			}
+		}
+	}
+}
